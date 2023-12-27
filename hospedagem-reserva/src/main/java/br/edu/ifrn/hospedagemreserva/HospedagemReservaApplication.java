cfc3ba98d854654857e70b939ee4a419fc451986
Alessandro Nunes <alessandro.nfx10@gmail.com>
2023-12-27 08:06:06 -0300

    primeiro commit

---
+++ hospedagem-reserva/src/main/java/br/edu/ifrn/hospedagemreserva/HospedagemReservaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.hospedagemreserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedagemReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedagemReservaApplication.class, args);
+	}
+
+}
---
+++ hospedagem-reserva/src/test/java/br/edu/ifrn/hospedagemreserva/HospedagemReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.hospedagemreserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedagemReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
